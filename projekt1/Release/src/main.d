4e3804338b7d8796636351154e1cec58e5739902
filip.matuszczak <filip.matuszczak@student.put.poznan.pl>
2018-09-23 13:55:05 +0200

    removed gitgnore

---
+++ projekt1/Release/src/main.d
@@ -0,0 +1,132 @@
+src/main.o: ../src/main.c \
+ C:/Users/oem/workspace/projekt1/CMSIS/device/stm32f4xx.h \
+ C:/Users/oem/workspace/projekt1/CMSIS/core/core_cm4.h \
+ C:/Users/oem/workspace/projekt1/CMSIS/core/core_cmInstr.h \
+ C:/Users/oem/workspace/projekt1/CMSIS/core/core_cmFunc.h \
+ C:/Users/oem/workspace/projekt1/CMSIS/core/core_cm4_simd.h \
+ C:/Users/oem/workspace/projekt1/CMSIS/device/system_stm32f4xx.h \
+ C:/Users/oem/workspace/projekt1/StdPeriph_Driver/inc/stm32f4xx_conf.h \
+ C:/Users/oem/workspace/projekt1/StdPeriph_Driver/inc/stm32f4xx_adc.h \
+ C:/Users/oem/workspace/projekt1/StdPeriph_Driver/inc/stm32f4xx_can.h \
+ C:/Users/oem/workspace/projekt1/StdPeriph_Driver/inc/stm32f4xx_crc.h \
+ C:/Users/oem/workspace/projekt1/StdPeriph_Driver/inc/stm32f4xx_cryp.h \
+ C:/Users/oem/workspace/projekt1/StdPeriph_Driver/inc/stm32f4xx_dac.h \
+ C:/Users/oem/workspace/projekt1/StdPeriph_Driver/inc/stm32f4xx_dbgmcu.h \
+ C:/Users/oem/workspace/projekt1/StdPeriph_Driver/inc/stm32f4xx_dcmi.h \
+ C:/Users/oem/workspace/projekt1/StdPeriph_Driver/inc/stm32f4xx_dma.h \
+ C:/Users/oem/workspace/projekt1/StdPeriph_Driver/inc/stm32f4xx_exti.h \
+ C:/Users/oem/workspace/projekt1/StdPeriph_Driver/inc/stm32f4xx_flash.h \
+ C:/Users/oem/workspace/projekt1/StdPeriph_Driver/inc/stm32f4xx_fsmc.h \
+ C:/Users/oem/workspace/projekt1/StdPeriph_Driver/inc/stm32f4xx_hash.h \
+ C:/Users/oem/workspace/projekt1/StdPeriph_Driver/inc/stm32f4xx_gpio.h \
+ C:/Users/oem/workspace/projekt1/StdPeriph_Driver/inc/stm32f4xx_i2c.h \
+ C:/Users/oem/workspace/projekt1/StdPeriph_Driver/inc/stm32f4xx_iwdg.h \
+ C:/Users/oem/workspace/projekt1/StdPeriph_Driver/inc/stm32f4xx_pwr.h \
+ C:/Users/oem/workspace/projekt1/StdPeriph_Driver/inc/stm32f4xx_rcc.h \
+ C:/Users/oem/workspace/projekt1/StdPeriph_Driver/inc/stm32f4xx_rng.h \
+ C:/Users/oem/workspace/projekt1/StdPeriph_Driver/inc/stm32f4xx_rtc.h \
+ C:/Users/oem/workspace/projekt1/StdPeriph_Driver/inc/stm32f4xx_sdio.h \
+ C:/Users/oem/workspace/projekt1/StdPeriph_Driver/inc/stm32f4xx_spi.h \
+ C:/Users/oem/workspace/projekt1/StdPeriph_Driver/inc/stm32f4xx_syscfg.h \
+ C:/Users/oem/workspace/projekt1/StdPeriph_Driver/inc/stm32f4xx_tim.h \
+ C:/Users/oem/workspace/projekt1/StdPeriph_Driver/inc/stm32f4xx_usart.h \
+ C:/Users/oem/workspace/projekt1/StdPeriph_Driver/inc/stm32f4xx_wwdg.h \
+ C:/Users/oem/workspace/projekt1/StdPeriph_Driver/inc/misc.h \
+ C:/Users/oem/workspace/projekt1/Utilities/stm32f4_discovery.h \
+ ../src/defines.h ../src/tm_stm32f4_dac_signal.h \
+ C:/Users/oem/workspace/projekt1/StdPeriph_Driver/inc/stm32f4xx_dac.h \
+ C:/Users/oem/workspace/projekt1/StdPeriph_Driver/inc/stm32f4xx_tim.h \
+ C:/Users/oem/workspace/projekt1/StdPeriph_Driver/inc/stm32f4xx_dma.h \
+ ../src/tm_stm32f4_timer_properties.h \
+ C:/Users/oem/workspace/projekt1/StdPeriph_Driver/inc/stm32f4xx_rcc.h \
+ ../src/tm_stm32f4_gpio.h \
+ C:/Users/oem/workspace/projekt1/StdPeriph_Driver/inc/stm32f4xx_gpio.h \
+ ../src/tm_stm32f4_delay.h
+
+C:/Users/oem/workspace/projekt1/CMSIS/device/stm32f4xx.h:
+
+C:/Users/oem/workspace/projekt1/CMSIS/core/core_cm4.h:
+
+C:/Users/oem/workspace/projekt1/CMSIS/core/core_cmInstr.h:
+
+C:/Users/oem/workspace/projekt1/CMSIS/core/core_cmFunc.h:
+
+C:/Users/oem/workspace/projekt1/CMSIS/core/core_cm4_simd.h:
+
+C:/Users/oem/workspace/projekt1/CMSIS/device/system_stm32f4xx.h:
+
+C:/Users/oem/workspace/projekt1/StdPeriph_Driver/inc/stm32f4xx_conf.h:
+
+C:/Users/oem/workspace/projekt1/StdPeriph_Driver/inc/stm32f4xx_adc.h:
+
+C:/Users/oem/workspace/projekt1/StdPeriph_Driver/inc/stm32f4xx_can.h:
+
+C:/Users/oem/workspace/projekt1/StdPeriph_Driver/inc/stm32f4xx_crc.h:
+
+C:/Users/oem/workspace/projekt1/StdPeriph_Driver/inc/stm32f4xx_cryp.h:
+
+C:/Users/oem/workspace/projekt1/StdPeriph_Driver/inc/stm32f4xx_dac.h:
+
+C:/Users/oem/workspace/projekt1/StdPeriph_Driver/inc/stm32f4xx_dbgmcu.h:
+
+C:/Users/oem/workspace/projekt1/StdPeriph_Driver/inc/stm32f4xx_dcmi.h:
+
+C:/Users/oem/workspace/projekt1/StdPeriph_Driver/inc/stm32f4xx_dma.h:
+
+C:/Users/oem/workspace/projekt1/StdPeriph_Driver/inc/stm32f4xx_exti.h:
+
+C:/Users/oem/workspace/projekt1/StdPeriph_Driver/inc/stm32f4xx_flash.h:
+
+C:/Users/oem/workspace/projekt1/StdPeriph_Driver/inc/stm32f4xx_fsmc.h:
+
+C:/Users/oem/workspace/projekt1/StdPeriph_Driver/inc/stm32f4xx_hash.h:
+
+C:/Users/oem/workspace/projekt1/StdPeriph_Driver/inc/stm32f4xx_gpio.h:
+
+C:/Users/oem/workspace/projekt1/StdPeriph_Driver/inc/stm32f4xx_i2c.h:
+
+C:/Users/oem/workspace/projekt1/StdPeriph_Driver/inc/stm32f4xx_iwdg.h:
+
+C:/Users/oem/workspace/projekt1/StdPeriph_Driver/inc/stm32f4xx_pwr.h:
+
+C:/Users/oem/workspace/projekt1/StdPeriph_Driver/inc/stm32f4xx_rcc.h:
+
+C:/Users/oem/workspace/projekt1/StdPeriph_Driver/inc/stm32f4xx_rng.h:
+
+C:/Users/oem/workspace/projekt1/StdPeriph_Driver/inc/stm32f4xx_rtc.h:
+
+C:/Users/oem/workspace/projekt1/StdPeriph_Driver/inc/stm32f4xx_sdio.h:
+
+C:/Users/oem/workspace/projekt1/StdPeriph_Driver/inc/stm32f4xx_spi.h:
+
+C:/Users/oem/workspace/projekt1/StdPeriph_Driver/inc/stm32f4xx_syscfg.h:
+
+C:/Users/oem/workspace/projekt1/StdPeriph_Driver/inc/stm32f4xx_tim.h:
+
+C:/Users/oem/workspace/projekt1/StdPeriph_Driver/inc/stm32f4xx_usart.h:
+
+C:/Users/oem/workspace/projekt1/StdPeriph_Driver/inc/stm32f4xx_wwdg.h:
+
+C:/Users/oem/workspace/projekt1/StdPeriph_Driver/inc/misc.h:
+
+C:/Users/oem/workspace/projekt1/Utilities/stm32f4_discovery.h:
+
+../src/defines.h:
+
+../src/tm_stm32f4_dac_signal.h:
+
+C:/Users/oem/workspace/projekt1/StdPeriph_Driver/inc/stm32f4xx_dac.h:
+
+C:/Users/oem/workspace/projekt1/StdPeriph_Driver/inc/stm32f4xx_tim.h:
+
+C:/Users/oem/workspace/projekt1/StdPeriph_Driver/inc/stm32f4xx_dma.h:
+
+../src/tm_stm32f4_timer_properties.h:
+
+C:/Users/oem/workspace/projekt1/StdPeriph_Driver/inc/stm32f4xx_rcc.h:
+
+../src/tm_stm32f4_gpio.h:
+
+C:/Users/oem/workspace/projekt1/StdPeriph_Driver/inc/stm32f4xx_gpio.h:
+
+../src/tm_stm32f4_delay.h:
